d34e6b148e51eb3801e9e4d732a957cae0f7c0a3
Adam Gray <adam.gray@netlify.com>
2023-11-01 11:34:12 -0500

    fix wrapping

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,10 +7,10 @@ interface Props {
 const Nav: React.FC<Props> = ({}) => {
   return (
     <div className='flex justify-between items-center px-8 py-8 bg-blue-100'>
-      <a className='font-bold' href="/">Netlify Create</a>
+      <a className='font-bold' href="/"><img src="/logo.svg" className='w-28'/></a>
       <ul className='flex gap-4'>
         <li>
-          <Link href="/about">about</Link>
+          <Link href="/about">About</Link>
         </li>
       </ul>
     </div>
--- components/Stats.tsx
+++ components/Stats.tsx
@@ -10,12 +10,12 @@ interface Props {
 const Stats: React.FC<Props> = ({ id, heading, body, stats }) => {
   return (
     <div className='' data-sb-object-id={id}>
-      <div className="container mx-auto px-2 py-20">
+      <div className="container mx-auto px-8 py-20">
         <div className="text-center">
           <h1 className='font-bold text-2xl' data-sb-field-path="heading">{heading}</h1>
           <p data-sb-field-path="body">{body}</p>
         </div>
-        <div className='flex justify-between text-center rounded mt-20'>
+        <div className='flex text-center gap-20 mt-20 justify-center flex-wrap'>
           {stats?.map((stat) => (
             <div key={stat.sys.id} data-sb-object-id={stat.sys.id} className='border shadow-xl gap-20 py-20 px-20 rounded-2xl'>
               <p className='font-bold text-4xl' data-sb-field-path="value">{stat.value}</p>
